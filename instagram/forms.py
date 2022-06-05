8079baf768c93eb1fcf014c69432663a9dc55ddb
charity-bit <charitynyanchera@gmail.com>
2022-06-05 20:56:37 +0300

    ft:post image form"

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,9 +1,19 @@
+from ctypes.wintypes import POINT
 from django.contrib.auth.forms import UserCreationForm
-
-
 from  django.contrib.auth.models import User
+from django import forms
+from .models import Post
 
 class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username','email','password1','password2')
+
+
+
+class PostForm(forms.Model):
+    class Meta:
+        model = Post
+        fields = ['image','post_name','post_caption']
+
+
